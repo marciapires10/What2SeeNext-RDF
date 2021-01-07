8171bdcefa12c4f21f25705ea2e01dc3b7851a37
marciapires <marcia.pires19@live.com>
2021-01-07 21:09:38 +0000

    Some queries

--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from s4api.graphdb_api import GraphDBApi
 from s4api.swagger import ApiClient
 
+IMAGES_SITE = "http://image.tmdb.org/t/p/w200"
 endpoint = "http://localhost:7200"
 repo_name = "movies_db"
 client = ApiClient(endpoint=endpoint)
@@ -13,20 +14,34 @@ accessor = GraphDBApi(client)
 # Create your views here.
 def index(request):
     # select top 10 movies
-    query = """"
-    PREFIX mov:<http://digital-media.com/pred/>
-    SELECT ?poster, ?name, ?rating
-    WHERE {
-    }
-    """
-    """payload_query = {"query": query}
+    query = """
+            PREFIX mov:<http://moviesProject.org/sub/mov/>
+            PREFIX pred:<http://moviesProject.org/pred/>
+            SELECT ?movie ?id ?poster ?title ?has_score
+            WHERE {
+            ?movie pred:poster ?id .
+            ?movie pred:poster ?poster .
+            ?movie pred:title ?title .
+            ?movie pred:has_score ?has_score .
+            }
+            ORDER BY DESC(?has_score) LIMIT 10
+            """
+    payload_query = {"query": query}
     res = accessor.sparql_select(body=payload_query,
                                  repo_name=repo_name)
+    print(res)
+
     res = json.loads(res)
     top_movies = []
     for e in res['results']['bindings']:
-        for v in e.values():
-            top_movies.append(v['value'])"""
+        movie_tmp = []
+        movie_tmp.append(e['id']['value'])
+        movie_tmp.append(e['title']['value'])
+        poster = IMAGES_SITE + str(e['poster']['value'])
+        movie_tmp.append(poster)
+        movie_tmp.append(e['has_score']['value'])
+        top_movies.append(movie_tmp)
+
 
     query = """"
         PREFIX ser:<http://digital-media.com/pred/>
@@ -43,38 +58,44 @@ def index(request):
         for v in e.values():
             top_series.append(v['value'])"""
 
-    #tparams = {
-      #  'movies' : top_movies,
+    tparams = {
+      'top_movies': top_movies,
       #  'series' : top_series,
-    #}
+    }
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', tparams)
 
 def movies(request):
+    #select all movies
+    query = """
+        PREFIX mov:<http://moviesProject.org/sub/mov/>
+        PREFIX pred:<http://moviesProject.org/pred/>
+        SELECT ?movie ?id ?poster ?title
+        WHERE {
+            ?movie pred:id ?id .
+            ?movie pred:poster ?poster .
+            ?movie pred:title ?title .
+        }"""
+    payload_query = {"query": query}
+    res = accessor.sparql_select(body=payload_query,
+                                 repo_name=repo_name)
 
-    movies = [];
-    movies.append(1);
-    movies.append(2);
-    movies.append(3);
-    movies.append(4);
-    movies.append(5);
-    movies.append(6);
-    movies.append(7);
-    movies.append(8);
-    movies.append(9);
-    movies.append(10);
-    movies.append(4);
-    movies.append(5);
-    movies.append(6);
-    movies.append(7);
-    movies.append(8);
-    movies.append(9);
-    movies.append(10);
+    res = json.loads(res)
+    movies_all = []
+    for e in res['results']['bindings']:
+        movie_tmp = []
+        movie_tmp.append(e['id']['value'])
+        movie_tmp.append(e['title']['value'])
+        poster = IMAGES_SITE + str(e['poster']['value'])
+        movie_tmp.append(poster)
+        movies_all.append(movie_tmp)
+
+    print(movies_all)
 
 
 
     tparams = {
-        'movies': movies,
+        'movies_all': movies_all,
     }
 
     return render(request, 'movies_list.html', tparams)
