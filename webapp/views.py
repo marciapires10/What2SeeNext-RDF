06bab5441bb62ac6c009ed3b0e7f38866185f016
marciapires <marcia.pires19@live.com>
2021-01-08 00:51:03 +0000

    No image available

--- webapp/views.py
+++ webapp/views.py
@@ -5,27 +5,28 @@ from s4api.graphdb_api import GraphDBApi
 from s4api.swagger import ApiClient
 
 IMAGES_SITE = "http://image.tmdb.org/t/p/w200"
+NO_IMAGE = "../static/assets/img/NoImage.jpg"
 endpoint = "http://localhost:7200"
 repo_name = "movies_db"
 client = ApiClient(endpoint=endpoint)
 accessor = GraphDBApi(client)
 
-
 # Create your views here.
-def index(request):
+
+def get_top_movies():
     # select top 10 movies
     query = """
-            PREFIX pred:<http://moviesProject.org/pred/>
-            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-            SELECT ?id ?poster ?title ?has_score
-            WHERE {
-                ?movie pred:id_m ?id .
-                ?movie pred:poster ?poster .
-                ?movie pred:title ?title .
-                ?movie pred:has_score ?has_score .
-            }
-            ORDER BY DESC(xsd:float(?has_score)) LIMIT 10
-            """
+                PREFIX pred:<http://moviesProject.org/pred/>
+                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                SELECT ?id ?poster ?title ?has_score
+                WHERE {
+                    ?movie pred:id_m ?id .
+                    ?movie pred:poster ?poster .
+                    ?movie pred:title ?title .
+                    ?movie pred:has_score ?has_score .
+                }
+                ORDER BY DESC(xsd:float(?has_score)) LIMIT 10
+                """
     payload_query = {"query": query}
     res = accessor.sparql_select(body=payload_query,
                                  repo_name=repo_name)
@@ -36,23 +37,29 @@ def index(request):
         movie_tmp = []
         movie_tmp.append(e['id']['value'])
         movie_tmp.append(e['title']['value'])
-        poster = IMAGES_SITE + str(e['poster']['value'])
+        if str(e['poster']['value']) is not "":
+            poster = IMAGES_SITE + str(e['poster']['value'])
+        else:
+            poster = NO_IMAGE
         movie_tmp.append(poster)
         movie_tmp.append(e['has_score']['value'])
         top_movies.append(movie_tmp)
 
+    return top_movies
+
+def get_top_series():
     query = """
-            PREFIX pred:<http://moviesProject.org/pred/>
-            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-            SELECT ?id ?poster ?title ?has_score
-            WHERE {
-                ?serie pred:id_s ?id .
-                ?serie pred:poster ?poster .
-                ?serie pred:title ?title .
-                ?serie pred:has_score ?has_score .
-            }
-            ORDER BY DESC(xsd:float(?has_score)) LIMIT 10
-            """
+                PREFIX pred:<http://moviesProject.org/pred/>
+                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                SELECT ?id ?poster ?title ?has_score
+                WHERE {
+                    ?serie pred:id_s ?id .
+                    ?serie pred:poster ?poster .
+                    ?serie pred:title ?title .
+                    ?serie pred:has_score ?has_score .
+                }
+                ORDER BY DESC(xsd:float(?has_score)) LIMIT 10
+                """
 
     payload_query = {"query": query}
     res = accessor.sparql_select(body=payload_query,
@@ -66,10 +73,20 @@ def index(request):
         serie_tmp = []
         serie_tmp.append(e['id']['value'])
         serie_tmp.append(e['title']['value'])
-        poster = IMAGES_SITE + str(e['poster']['value'])
+        if str(e['poster']['value']) is not "":
+            poster = IMAGES_SITE + str(e['poster']['value'])
+        else:
+            poster = NO_IMAGE
         serie_tmp.append(poster)
         serie_tmp.append(e['has_score']['value'])
         top_series.append(serie_tmp)
+
+    return top_series
+
+def index(request):
+
+    top_movies = get_top_movies()
+    top_series = get_top_series()
 
     tparams = {
       'top_movies': top_movies,
@@ -123,7 +140,10 @@ def movies(request):
         movie_tmp = []
         movie_tmp.append(e['id']['value'])
         movie_tmp.append(e['title']['value'])
-        poster = IMAGES_SITE + str(e['poster']['value'])
+        if str(e['poster']['value']) is not "":
+            poster = IMAGES_SITE + str(e['poster']['value'])
+        else:
+            poster = NO_IMAGE
         movie_tmp.append(poster)
         movies_all.append(movie_tmp)
 
@@ -159,7 +179,10 @@ def series(request):
         serie_tmp = []
         serie_tmp.append(e['id']['value'])
         serie_tmp.append(e['title']['value'])
-        poster = IMAGES_SITE + str(e['poster']['value'])
+        if str(e['poster']['value']) is not "":
+            poster = IMAGES_SITE + str(e['poster']['value'])
+        else:
+            poster = NO_IMAGE
         serie_tmp.append(poster)
         series_all.append(serie_tmp)
 
